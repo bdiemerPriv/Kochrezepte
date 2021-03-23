a96a14e506fe3319e9806fa9ebe6d34400352831
Bernhard Diemer <bd@nedgex.com>
2021-03-23 15:13:34 +0100

    Initial Commit

---
+++ Kochrezepte/KochrezepteApp.swift
@@ -0,0 +1,17 @@
+//
+//  KochrezepteApp.swift
+//  Kochrezepte
+//
+//  Created by Bernhard Diemer  on 23.03.21.
+//
+
+import SwiftUI
+
+@main
+struct KochrezepteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
